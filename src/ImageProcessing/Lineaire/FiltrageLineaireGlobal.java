659b373bc546836befbb4dd24772b7e75a07b836
Actariss <l.naa@student.helmo.be>
2025-05-14 11:52:46 +0200

    Commit sudoku Butterworth implementation in progress

--- src/ImageProcessing/Lineaire/FiltrageLineaireGlobal.java
+++ src/ImageProcessing/Lineaire/FiltrageLineaireGlobal.java
@@ -135,8 +135,62 @@ public class FiltrageLineaireGlobal {
     public static int[][] filtrePasseBasButterworth(int[][] image, int frequenceCoupure, int ordre) {
         // TODO : FFT + création du filtre Butterworth passe-bas
         // TODO : Application du filtre + iFFT
-        return null;
+        int M = image.length;
+        int N = image[0].length;
+
+        // Convertir l'image en matrice de doubles au lieu de Int 
+        double[][] f = new double[M][N];
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                f[i][j] = (double) image[i][j];
+            }
+        }
+        
+        MatriceComplexe fourier = Fourier.Fourier2D(f);
+        
+        fourier = Fourier.decroise(fourier);
+      
+        for (int u = 0; u < M; u++) {
+            for (int v = 0; v < N; v++) {
+                int centreM = M / 2;
+                int centreN = N / 2;    
+                // Calculer la distance au centre
+                double distance = Math.sqrt((u - centreM) * (u - centreM) + (v - centreN) * (v - centreN));
+                
+                // Passage dans la fonction de transfert
+                
+                double H = 1.0 / (1.0 + Math.pow(Math.sqrt(u*u+v*v)/frequenceCoupure, 2*ordre));
+                if (distance > frequenceCoupure) {    
+                    fourier.set(u, v, u*H, v*H);
+                }
+                
+            }
+        }
+        
+        // fourier = Fourier.decroise(fourier);
+        
+        //MatriceComplexe resultatComplex = Fourier.InverseFourier2D(fourier);
+        MatriceComplexe resultatComplex = fourier;
+        
+        double[][] resultatDouble = resultatComplex.getPartieReelle();
+        int[][] result = new int[M][N];
+        
+        // Normalisation et conversion en entiers
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                
+                int valeur = (int) Math.round(resultatDouble[i][j]);
+                // Limiter les valeurs entre 0 et 255 (Pour une img sinon caca)
+                if (valeur < 0) valeur = 0;
+                if (valeur > 255) valeur = 255;
+                result[i][j] = valeur;
+            }
+        }
+        
+        return result;
+        
     }
+    
 
     /**
      * Applique un filtre passe-haut de Butterworth à l'image.
