0fe33b42ecfaa7337c3eaa0d2ed43a956d635169
Actariss <l.naa@student.helmo.be>
2025-05-14 11:53:10 +0200

    Commit sudoku Butterworth implementation in progress

--- src/isilimageprocessing/IsilImageProcessing.java
+++ src/isilimageprocessing/IsilImageProcessing.java
@@ -697,7 +697,22 @@ public class IsilImageProcessing extends javax.swing.JFrame implements ClicListe
     }//GEN-LAST:event_jMenuItemOuvrirRGBActionPerformed
 
     private void jMenuItemPasseBasButterworthActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItemPasseBasButterworthActionPerformed
-        // TODO add your handling code here:
+        try {
+            // TODO add your handling code here:
+            // Demander la fréquence de coupure
+            String input = JOptionPane.showInputDialog(this, "Fréquence de coupure :");
+            if (input == null) return; // Annulé
+            int fc = Integer.parseInt(input);
+            var img = imageNG.getMatrice();
+            int[][] matriceFiltree = FiltrageLineaireGlobal.filtrePasseBasButterworth(img, fc, 1);
+            CImageNG imagefiltree = new CImageNG(matriceFiltree);
+            observer.setCImage(imagefiltree);
+            // Appel au traitement (à adapter à ton système d’image)
+            //int[][] resultat = FiltrageLineaireGlobal.filtrePasseBasIdeal(imageActuelle, fc);
+            //afficherImage(resultat);
+        } catch (CImageNGException ex) {
+            Logger.getLogger(IsilImageProcessing.class.getName()).log(Level.SEVERE, null, ex);
+        }           
     }//GEN-LAST:event_jMenuItemPasseBasButterworthActionPerformed
 
     private void jMenuItemPasseHautButterworthActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItemPasseHautButterworthActionPerformed
