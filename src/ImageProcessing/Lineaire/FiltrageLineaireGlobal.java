3e7029f6f735ad97436362abab0523cea8abbee3
Dylan <proddylan@gmail.com>
2025-05-15 10:58:46 +0200

    Implementation du filtrePasseHautButterworth

--- src/ImageProcessing/Lineaire/FiltrageLineaireGlobal.java
+++ src/ImageProcessing/Lineaire/FiltrageLineaireGlobal.java
@@ -199,8 +199,60 @@ public class FiltrageLineaireGlobal {
      * @return Image filtrée.
      */
     public static int[][] filtrePasseHautButterworth(int[][] image, int frequenceCoupure, int ordre) {
-        // TODO : FFT + création du filtre Butterworth passe-haut
-        // TODO : Application du filtre + iFFT
-        return null;
+        int M = image.length;
+        int N = image[0].length;
+
+        // Convertir l'image en matrice de doubles au lieu de Int 
+        double[][] f = new double[M][N];
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                f[i][j] = (double) image[i][j];
+            }
+        }
+
+        MatriceComplexe fourier = Fourier.Fourier2D(f);
+
+        fourier = Fourier.decroise(fourier);
+
+        int centreM = M / 2;
+        int centreN = N / 2;
+
+        // Appliquer le filtre de Butterworth passe-bas
+        for (int u = 0; u < M; u++) {
+            for (int v = 0; v < N; v++) {
+                // Calculer la distance au centre
+                double distance = Math.sqrt((u - centreM) * (u - centreM) + (v - centreN) * (v - centreN));
+
+                // Calculer la fonction de transfert de Butterworth 
+                double H = 1.0 / (1.0 + Math.pow(frequenceCoupure / distance, 2 * ordre));
+
+                // Multiplier le spectre par la fonction de transfert
+                Complexe valeur = fourier.get(u, v);
+                double partieReelle = valeur.getPartieReelle() * H;
+                double partieImaginaire = valeur.getPartieImaginaire() * H;
+
+                fourier.set(u, v, partieReelle, partieImaginaire);
+            }
+        }
+
+        fourier = Fourier.decroise(fourier);
+
+        MatriceComplexe resultatComplex = Fourier.InverseFourier2D(fourier);
+
+        double[][] resultatDouble = resultatComplex.getPartieReelle();
+        int[][] result = new int[M][N];
+
+        // Normalisation et conversion en entiers
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                int valeur = (int) Math.round(resultatDouble[i][j]);
+                // Limiter les valeurs entre 0 et 255 (Pour une img sinon caca)
+                if (valeur < 0) valeur = 0;
+                if (valeur > 255) valeur = 255;
+                result[i][j] = valeur;
+            }
+        }
+
+        return result;
     }
 }
