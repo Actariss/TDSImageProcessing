7015b298dfd2b3b4806b362975bde017ca4c0eec
Dylan <proddylan@gmail.com>
2025-05-17 14:21:55 +0200

    Added the FiltrageLineaireLocal class

---
+++ src/ImageProcessing/Lineaire/FiltrageLineaireLocal.java
@@ -0,0 +1,26 @@
+package ImageProcessing.Lineaire;
+
+public class FiltrageLineaireLocal {
+
+    /**
+     * Réalise le filtrage local de l'image en utilisant le masque de convolution.
+     * @param image Image d'entrée en niveaux de gris (matrice 2D d'entiers).
+     * @param masque Masque de convolution (matrice 2D de doubles).
+     * @return Image filtrée.
+     */
+    public static int[][] filtreMasqueConvolution(int[][] image, double[][] masque) {
+        // TODO: Implement
+        return null;
+    }
+    
+    /**
+     * Réalise un filtrage moyenneur de l'image avec un masque de convolution carré.
+     * @param image Image d'entrée en niveaux de gris.
+     * @param tailleMasque Taille du masque carré (doit être impaire).
+     * @return Image filtrée.
+     */
+    public static int[][] filtreMoyenneur(int[][] image, int tailleMasque) {
+        // TODO: Implement
+        return null;
+    }
+}
