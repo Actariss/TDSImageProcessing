8e167ee2fc23c8f3b86414329fea6f433bc63a3e
Dylan <proddylan@gmail.com>
2025-06-14 15:42:27 +0200

    Implementation de l'application 6

--- src/ImageProcessing/Applications/Applications.java
+++ src/ImageProcessing/Applications/Applications.java
@@ -7,12 +7,16 @@ import CImage.Exceptions.CImageRGBException;
 import ImageProcessing.Histogramme.Histogramme;
 import ImageProcessing.NonLineaire.MorphoComplexe;
 import ImageProcessing.NonLineaire.MorphoElementaire;
+import ImageProcessing.Contours.ContoursNonLineaire;
+import ImageProcessing.Contours.ContoursLineaire;
 import ImageProcessing.Seuillage.Seuillage;
 import ImageProcessing.Utils.Utils;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.io.File;
+import java.io.IOException;
 
 public class Applications 
 {
@@ -184,9 +188,98 @@ public class Applications
 
     }
     
-    public static void application6(int[][] image)
+    public static Map<String,CImageRGB> application6(int[][] imageVaisseaux, CImageRGB vaisseauxRGB, CImageRGB planeteRGB) 
+        throws CImageRGBException, CImageNGException
     {
-        // TODO
+        // Seuillage initial pour isoler les vaisseaux
+        int[][] seuil60 = Seuillage.seuillageSimple(imageVaisseaux, 60);
+
+        // Érosion forte pour ne garder que le gros vaisseau
+        int[][] eroded = MorphoElementaire.erosion(seuil60, 21);
+
+        // Reconstruction du gros vaisseau
+        int[][] recon = MorphoComplexe.reconstructionGeodesique(eroded, seuil60);
+
+        // Soustraction pour obtenir le petit vaisseau
+        int[][] petitVaisseau = soustraction(seuil60, recon);
+
+        // Nettoyage du petit vaisseau
+        int[][] opened = MorphoElementaire.ouverture(petitVaisseau, 13);
+
+        // ✅ Reconstruction sur **imageVaisseaux** pour un masque complet
+        int[][] finalMask = MorphoComplexe.reconstructionGeodesique(opened, imageVaisseaux);
+
+        // ✅ Fermeture plus forte pour combler les trous
+        int[][] masquePlein = MorphoElementaire.fermeture(finalMask, 9);
+        masquePlein = Seuillage.seuillageSimple(masquePlein, 30);
+
+        // ✅ Détection des contours avec gradient 
+        int[][] contour = ContoursNonLineaire.gradientBeucher(masquePlein);
+        contour = Seuillage.seuillageSimple(contour, 1);
+        contour = MorphoElementaire.dilatation(contour, 3); // pour l'épaissir un peu
+
+        // Copier le vaisseau sur la planète
+        CImageRGB synthese = copierVaisseauSimple(planeteRGB, vaisseauxRGB, masquePlein);
+
+        // Ajouter le contour rouge propre
+        CImageRGB synthese2 = ajouterContourRouge(synthese, contour);
+
+        Map<String,CImageRGB> res = new HashMap<>();
+        res.put("SYN1", synthese);
+        res.put("SYN2", synthese2);
+        return res;
+    }
+
+    private static CImageRGB ajouterContourRouge(CImageRGB image, int[][] contour) throws CImageRGBException {
+        int l = image.getLargeur();
+        int h = image.getHauteur();
+        int[][] r = new int[l][h];
+        int[][] g = new int[l][h];
+        int[][] b = new int[l][h];
+        image.getMatricesRGB(r, g, b);
+
+        // Parcourir et appliquer le contour rouge
+        for (int i = 0; i < l; i++) {
+            for (int j = 0; j < h; j++) {
+                if (contour[i][j] > 0) {
+                    // Appliquer un rouge pur pour le contour
+                    r[i][j] = 255;
+                    g[i][j] = 0;
+                    b[i][j] = 0;
+                }
+            }
+        }
+
+        return new CImageRGB(r, g, b);
+    }
+    
+    private static CImageRGB copierVaisseauSimple(CImageRGB planete, CImageRGB vaisseau, int[][] masque) 
+        throws CImageRGBException {
+        int l = planete.getLargeur();
+        int h = planete.getHauteur();
+
+        int[][] rP = new int[l][h];
+        int[][] gP = new int[l][h];
+        int[][] bP = new int[l][h];
+        planete.getMatricesRGB(rP, gP, bP);
+
+        int[][] rV = new int[l][h];
+        int[][] gV = new int[l][h];
+        int[][] bV = new int[l][h];
+        vaisseau.getMatricesRGB(rV, gV, bV);
+
+        // Copie simple : si le masque > 0, prendre le vaisseau
+        for (int i = 0; i < l; i++) {
+            for (int j = 0; j < h; j++) {
+                if (masque[i][j] > 0) {
+                    rP[i][j] = rV[i][j];
+                    gP[i][j] = gV[i][j];
+                    bP[i][j] = bV[i][j];
+                }
+            }
+        }
+
+        return new CImageRGB(rP, gP, bP);
     }
     
     public static void application7(int[][] image)
